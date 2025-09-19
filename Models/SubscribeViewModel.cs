07dd8d18854574f2caee6af02a2ee18a3f62ef78
onlypd <idspotify9@gmail.com>
2025-09-20 00:06:09 +0530

    Reset repo

---
+++ Models/SubscribeViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UnsoocialLandingPage.Models
+{
+	public class SubscribeViewModel
+	{
+		[Required(ErrorMessage = "Please enter your email address.")]
+		[EmailAddress(ErrorMessage = "Please enter a valid email address.")]
+		public string Email { get; set; }
+	}
+}
